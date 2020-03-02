fe15f0ea628b90ba6325f24d42552a90db1dcc0f
sunflower <17307130191@fudan.edu.cn>
2020-03-02 23:42:25 +0800

    instance

--- assignment-1/benchtest/cpu_tb.sv
+++ assignment-1/benchtest/cpu_tb.sv
@@ -15,9 +15,9 @@ string summary;
 integer fans, frun, fimem, fdmem, error_count, imem_counter, dmem_counter;
 
 // module instances
-mips mips(cpu_clk, reset, pc, instr, cpu_mem_write, cpu_data_addr, write_data, read_data);
-imem #(ISIZE) imem(pc[7:2], instr);
-dmem #(DSIZE) dmem(clk, mem_write, cpu_data_addr, write_data, read_data);
+mips mips(.clk(cpu_clk), .reset(reset), .pc(pc), .instr(instr), .memwrite(cpu_mem_write), .aluout(cpu_data_addr), .writedata(write_data), .readdata(read_data));
+imem #(ISIZE) imem(.a(pc[7:2]), .rd(instr));
+dmem #(DSIZE) dmem(.clk(clk), .memwrite(mem_write), .a(cpu_data_addr), .writedata(write_data), .rd(read_data));
 
 // clock and reset
 always #20 clk = ~clk;
@@ -64,6 +64,7 @@ task judge_memory(
     end
 endtask
 
+// check runtime
 task runtime_checker(
     input integer frun
 );
@@ -93,6 +94,7 @@ begin
 	$finish;
 end
 
+// init memory
 task init(input string name);
     imem.RAM = '{ default: '0 };
     dmem.RAM = '{ default: '0 };

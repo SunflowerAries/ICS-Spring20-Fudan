39a57e3fecae88f82d9a3f8511b8d43b98ab96ed
sunflower <17307130191@fudan.edu.cn>
2020-04-27 18:23:43 +0800

    check

---
+++ assignment-2/check.sh
@@ -0,0 +1,19 @@
+unzip -oq $1
+echo $1
+stuId=$(grep -o "^[0-9]*" <<< $1)
+echo $stuId
+if [ ! -d $stuId ]; then
+	echo 解压缩文件中需要包一层 $stuId/ 文件夹
+else
+	cd $stuId
+	if [ ! -d Pipeline ]; then
+		echo 项目名为 Pipeline
+	else
+		cd Pipeline
+		if [ ! -f makefile ]; then
+			echo 添加 makefile
+		elif [ ! -d benchtest ]; then
+			echo 添加 benchtest
+		fi
+	fi
+fi
